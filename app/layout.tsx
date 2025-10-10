637a0bd5529500139d8e239873ad4d0e291efcfe
mamylahi <dembouzfaye7@gmail.com>
2025-10-10 17:33:03 +0000

    publ

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
       <head>
           <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js"/>
           <script dangerouslySetInnerHTML={{__html: 'eruda.init();'}}/>
+          <script async
+                  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7432114644358102"
+                  crossOrigin="anonymous"></script>
           <meta name="google-adsense-account" content="ca-pub-7432114644358102"/>
       </head>
       <body className={`font-sans ${GeistSans.variable} ${playfair.variable}`}>
